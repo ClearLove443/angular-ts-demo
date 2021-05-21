d2d256e1de3bc239b5c9e945a7f29a83fd611dc7
qiangqiang.a.zhang <qiangqiang.a.zhang@accenture.com>
2021-05-21 19:00:33 +0800

    add

--- src/app/app-common/common-select_delete_update.component.ts
+++ src/app/app-common/common-select_delete_update.component.ts
@@ -1,46 +1,59 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { CommonService } from '@app/service/common.service';
 import { PcServiceService } from '@app/service/PcServiceService';
 import { AgGridAngular } from 'ag-grid-angular';
-import { BtnCellRenderer } from './btn-cell-renderer';
+import { ButtonRendererComponent } from './button-renderer.component';
 
 @Component({
   selector: 'app-common-select-delete-update',
   templateUrl: './common-select_delete_update.component.html',
-  styleUrls: ['./common-select_delete_update.component.css']
+  styleUrls: ['./common-select_delete_update.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CommonSelectDeleteUpdateComponent implements OnInit {
 
   rowData!: any;
   defaultColDef!: any;
   frameworkComponents!: any;
+  rowDataClicked1 = {};
   gridOptions = {
-    onRowClicked: (event: any) => console.log('A row was clicked'),
+    pagination: true,
+    page: '页'
   };
-  columnDefs = [
-    {headerName : 'Options',cellRenderer : 'buttonRenderer' ,
-    template : `
-    <button type="button" (click)="alertDir($event)">Alert</button>
-    `
-    },
-    { field: 'id'},
-    // { field: 'id', sortable: true, checkboxSelection: true },
-    { field: 'nickName' },
-    { field: 'userSex' },
-    this.frameworkComponents = {
-      btnCellRenderer: BtnCellRenderer
-    }
-  ];
+  // gridOptions = {
+  //   onRowClicked: (event: any) => console.log('A row was clicked'),
+  // };
+  columnDefs!: any;
   form!: any;
+  gridApi!: any;
+  gridColumnApi!: any;
   @ViewChild('agGrid') agGrid!: AgGridAngular;
 
   constructor(private commonService: CommonService, public service: PcServiceService) {
 
+    this.columnDefs = [
+      {
+        field: 'athlete',
+        cellRenderer: 'buttonRenderer',
+        cellRendererParams: {
+          onClick: this.onBtnClick.bind(this),
+          label: 'Click 1'
+        }
+      },
+      { field : 'id'},
+      // { field: 'id', sortable: true, checkboxSelection: true },
+      { field: 'nickName' },
+      { field: 'userSex' }
+    ];
+    this.frameworkComponents = {
+      buttonRenderer: ButtonRendererComponent,
+    };
     this.defaultColDef = {
       flex: 1,
       minWidth: 110,
-      editable: true,
+      editable: false,
       resizable: true,
+      pagination: false
     };
 
     // 订阅消息
@@ -48,22 +61,71 @@ export class CommonSelectDeleteUpdateComponent implements OnInit {
       this.form = text;
     });
   }
+  onBtnClick(e: { rowData: {}; }): void {
+    this.rowDataClicked1 = e.rowData;
+    alert(JSON.stringify(this.rowDataClicked1));
+  }
 
-  ageCellRendererFunc(params: any): string {
-    params.$scope.ageClicked = this.ageClicked;
-    return '<button ng-click="ageClicked(data.age)" ng-bind="data.age"></button>';
+  onGridReady(params: any): void {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
   }
-  ageClicked(age: string): void {
-    window.alert('Age clicked: ' + age);
-  }
+
   ngOnInit(): void {
   }
+
+  onPaginationChanged(): void {
+    console.log('onPaginationPageLoaded');
+    if (this.gridApi) {
+      this.setText('#lbLastPageFound', this.gridApi.paginationIsLastPageFound());
+      this.setText('#lbPageSize', this.gridApi.paginationGetPageSize());
+      this.setText('#lbCurrentPage', this.gridApi.paginationGetCurrentPage() + 1);
+      this.setText('#lbTotalPages', this.gridApi.paginationGetTotalPages());
+      // this.setLastButtonDisabled(!this.gridApi.paginationIsLastPageFound());
+    }
+  }
+
+  setText(selector: string, text: any): void {
+    const selectorStr = document.querySelector(selector);
+    if (!!selectorStr) {
+      selectorStr.innerHTML = text;
+    }
+  }
+
+  // setLastButtonDisabled(disabled: any): void {
+  //   const selectorStr = document.querySelector('#btLast');
+  //   if (!!selectorStr) {
+  //     // selectorStr.disabled = disabled;
+  //   }
+  // }
+
+  onBtFirst(): void {
+    this.gridApi.paginationGoToFirstPage();
+  }
+
+  onBtLast(): void {
+    this.gridApi.paginationGoToLastPage();
+  }
+
+  onBtNext(): void {
+    this.gridApi.paginationGoToNextPage();
+  }
+
+  onBtPrevious(): void {
+    this.gridApi.paginationGoToPreviousPage();
+  }
+
+  onBtPageFive(): void {
+    this.gridApi.paginationGoToPage(4);
+  }
+
   searchByCondition(): void {
     this.commonService.searchByCondition(this.form).subscribe(val => {
       console.log(val);
       this.rowData = val;
     });
   }
+
   getSelectedRows(): any[] {
     const selectedNodes = this.agGrid.api.getSelectedNodes();
     const selectedData = selectedNodes.map(node => node.data);
@@ -99,14 +161,8 @@ export class CommonSelectDeleteUpdateComponent implements OnInit {
     },(error: Error) =>  alert(error.message));
   }
 
-  onRowSelect(event: { data: any; }) {
-    // console.log(event.data)
-    // alert(event.data)
-  }
-
-  alertDir(event: { data: any; }) {
-    console.log(event.data)
-    alert(event.data)
-    // here I want which row is selected to delete
+  onRowSelect(event: { data: any; }): void {
+    console.log(event.data);
+    alert(event.data);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
-import { BtnCellRenderer } from './app-common/btn-cell-renderer';
+import { AgGridModule } from 'ag-grid-angular';
+import { ButtonRendererComponent } from './app-common/button-renderer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CommonService } from './service/common.service';
 import { PcServiceService } from './service/PcServiceService';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BtnCellRenderer
+    ButtonRendererComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    AgGridModule.withComponents(ButtonRendererComponent),
     IonicModule.forRoot()
   ],
 
